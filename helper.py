33b134d3bd7366dde720b1c0f066efcd0f5b0bcf
Alex Shan <36291011+shan-mx@users.noreply.github.com>
2022-07-29 12:27:40 +0800

    feat: cut video result

--- helper.py
+++ helper.py
@@ -1,59 +1,48 @@
-import base64
-import cv2
-import subprocess
 from clip_client import Client
-from typing import List, Tuple, Dict
 from docarray import Document, DocumentArray
-import numpy as np
+import skvideo.io
+import imagehash
+from PIL import Image
+from numpy import ndarray
 
 client = Client(server='grpc://0.0.0.0:51000')
 
 
-def get_frame_types(file: str):
-    command = 'ffprobe -v error -show_entries frame=pict_type -of default=noprint_wrappers=1'.split()
-    out = subprocess.check_output(command + [file]).decode()
-    frame_types = out.replace('pict_type=', '').split()
-    return zip(range(len(frame_types)), frame_types)
+def get_keyframes(video_data: 'ndarray', cut_sim: float):
+    last_hash = imagehash.phash(Image.fromarray(video_data[0]))
+    i_frames = [0]
+    frame_num = 0
+    for each_frame in video_data:
+        frame_hash = imagehash.phash(Image.fromarray(each_frame))
+        similarity = 1 - (last_hash - frame_hash)/len(frame_hash.hash)**2
+        if similarity < cut_sim:
+            i_frames.append(frame_num)
+        frame_num += 1
+        last_hash = frame_hash
+    return i_frames
 
 
-def get_keyframes_b64(file: str, save_image: bool = False):
-    frame_types = get_frame_types(file)
-    i_frames = [x[0] for x in frame_types if x[1] == 'I']  # select the key frames
-    keyframes_b64 = []
-    if i_frames:
-        cap = cv2.VideoCapture(file)
-        for frame_num in i_frames:
-            cap.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
-            success, frame = cap.read()
-            if success:
-                if save_image:
-                    outname = str(frame_num) + '.jpg'
-                    cv2.imwrite(outname, frame)
-                    print('Saved: ' + outname)
-                retval, buffer = cv2.imencode('.jpg', frame)
-                pic_b64 = base64.b64encode(buffer).decode()  # get b64 string from ndarray
-                keyframes_b64.append((frame_num, pic_b64))
-        cap.release()
-    return keyframes_b64
+def get_keyframes_data(file: str, cut_sim: float):
+    video_data = read_video(file)
+    key_frames = get_keyframes(video_data, cut_sim)
+    video_length = len(video_data)
+    key_frames.append(video_length)
+    keyframes_data = [((i, key_frames[key_frames.index(i)+1]), video_data[i]) for i in key_frames if i != video_length]
+    return keyframes_data
 
 
-def search_frame(file_name: str, prompt: str, topn: int):
-    keyframe_data = get_keyframes_b64(file_name)
-    da = DocumentArray(
-        [Document(tags={'index': str(tup[0])}, uri='data:image/jpg;base64,' + tup[1]) for tup in keyframe_data])
-    d = Document(
-        text=prompt,
-        matches=da,
-    )
+def search_frame(file_name: str, prompt: str, topn: int, cut_sim: float):
+    keyframe_data = get_keyframes_data(file_name, cut_sim)
+    da = DocumentArray([Document(tags={'left': str(tup[0][0]), 'right': str(tup[0][1])}, tensor=tup[1]) for tup in keyframe_data])
+    d = Document(text=prompt, matches=da)
     r = client.rank([d])
-    result = r['@m', ['tags', 'uri', 'scores__clip_score__value']]
+    result = r['@m', ['tags', 'tensor', 'scores__clip_score__value']]
     return [each[:topn] for each in result]
 
 
-def get_img_np_from_b64(img):
-    b, g, r = cv2.split(cv2.imdecode(np.fromstring(base64.b64decode(img), dtype=np.uint8), cv2.IMREAD_COLOR))
-    return cv2.merge([r, g, b])  # convert cv2's BGR to PIL's RGB
+def read_video(file: str):
+    return skvideo.io.vread(file)
 
 
-if __name__ == '__main__':
-    print(search_frame('1.mp4', '一群人站在一起', 5))
+def write_video(file: str, data: 'ndarray'):
+    skvideo.io.vwrite(file, data)
--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import streamlit as st
-from helper import search_frame, get_img_np_from_b64
-st.set_page_config(page_title="CLIP Video Indexer", page_icon="🔍")
+from helper import search_frame, cut_results, read_video, write_video
+import os
+st.set_page_config(page_title='CLIP Video Indexer', page_icon='🔍')
 st.title('CLIP Video Indexer')
-uploaded_file = st.file_uploader("Choose a file")
-video_data = ''
-text_prompt = st.text_input("Text Prompt", "一群人站在一起")
-topn_value = st.text_input("Top N", "5")
-search_button = st.button("Search")
+uploaded_file = st.file_uploader('Choose a file')
+text_prompt = st.text_input('Text Prompt', '')
+topn_value = st.text_input('Top N', '5')
+cut_sim_value = st.text_input('Cut Sim', '0.6')
+search_button = st.button('Search')
 if search_button:
     if uploaded_file is not None:
-        with st.spinner("Processing..."):
-            candidates, b64, scores = search_frame(uploaded_file.name, text_prompt, int(topn_value))
-            for each in candidates:
-                i = candidates.index(each)
-                st.image(get_img_np_from_b64(b64[i][22:]), caption=str(scores[i]))  # [22:] to cut the prefix for CaS
-        st.success("Done!")
+        with st.spinner('Processing...'):
+            spams, ndarray, scores = search_frame(uploaded_file.name, text_prompt, int(topn_value), float(cut_sim_value))
+            video_data = read_video(uploaded_file.name)
+            os.makedirs('tmp_videos', exist_ok=True)
+            for spam in spams:
+                i = spams.index(spam)
+                save_name = 'tmp_videos/' + str(i) + '_tmp.mp4'
+                write_video(save_name, video_data[int(spam['left']):int(spam['right'])])
+                st.video(save_name)
+                os.remove(save_name)
+        st.success('Done!')
     else:
-        st.write("Please upload your video first")
+        st.write('Please upload your video first')
